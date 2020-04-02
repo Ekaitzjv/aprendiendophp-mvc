d8a1bf05255cecb383eac7cdb10af2b9ca9ab839
Ekaitz Jimenez <49812227+Ekaitzjv@users.noreply.github.com>
2020-04-02 23:20:09 +0200

    1 arreglo

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
 <?php
 require_once 'controllers/usuario.php';
 
-if(isset($_GET['controller']) && class_exists($GET_['controller'])){
-    $nombre_controlador = $_GET['controller'];
-    $controlador = new $nombre_controlador();
-
-    if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
-        $action = $_GET['action'];
-        $controlador->$action();
-        
-    }else{
-        echo "La pagina no existe";
-    }
-
+if(isset($_GET['controller'])){
+	$nombre_controlador = $_GET['controller'].'Controller';
 }else{
-    echo "La pagina no existe";
+	echo "La pagina que buscas no existe";
+	exit();
+}
+
+if(class_exists($nombre_controlador)){	
+	$controlador = new $nombre_controlador();
+	
+	if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+		$action = $_GET['action'];
+		$controlador->$action();
+	}else{
+		echo "La pagina que buscas no existe";
+	}
+}else{
+	echo "La pagina que buscas no existe";
 }
